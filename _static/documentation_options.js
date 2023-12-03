77083e84dd9fd5ae912342c6fb0bf28f72096fa6
return42 <markus.heiser@darmarIT.de>
2023-12-03 12:49:21 +0000

    build from commit 3829c253ff86eb169966c6b6e8c4247b774c68cb

---
+++ _static/documentation_options.js
@@ -0,0 +1,13 @@
+const DOCUMENTATION_OPTIONS = {
+    VERSION: '2023.12.3+3829c253f',
+    LANGUAGE: 'en',
+    COLLAPSE_INDEX: false,
+    BUILDER: 'html',
+    FILE_SUFFIX: '.html',
+    LINK_SUFFIX: '.html',
+    HAS_SOURCE: true,
+    SOURCELINK_SUFFIX: '.txt',
+    NAVIGATION_WITH_KEYS: false,
+    SHOW_SEARCH_SUMMARY: true,
+    ENABLE_SEARCH_SHORTCUTS: true,
+};
